6a155abffcc6316b9b0479b90453fd263a212dd4
Simon Gaus <phisto05@gmail.com>
2020-04-07 01:26:25 +0200

    Initial Commit

---
+++ StrorageIndicatorKit/StrorageIndicatorKit.h
@@ -0,0 +1,19 @@
+//
+//  StrorageIndicatorKit.h
+//  StrorageIndicatorKit
+//
+//  Created by Simon Gaus on 07.04.20.
+//  Copyright © 2020 Simon Gaus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StrorageIndicatorKit.
+FOUNDATION_EXPORT double StrorageIndicatorKitVersionNumber;
+
+//! Project version string for StrorageIndicatorKit.
+FOUNDATION_EXPORT const unsigned char StrorageIndicatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StrorageIndicatorKit/PublicHeader.h>
+
+
